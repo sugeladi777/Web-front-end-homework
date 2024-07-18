a03a664304261a78257f5e990577637266eb2b09
Ming <ssz21@mails.tsinghua.edu.cn>
2024-07-18 14:11:34 +0800

    完成了闹钟功能（尚未在主页上添加跳转按钮）

--- main.js
+++ main.js
@@ -7,19 +7,32 @@ function updateClock() {
   const milliseconds = parseFloat(now.getMilliseconds());
 
   // 获取dom树节点
-  const hourHand = document.getElementById('hour-hand');
-  const minuteHand = document.getElementById('minute-hand');
-  const secondHand = document.getElementById('second-hand');
+  const hourHand = document.getElementById("hour-hand");
+  const minuteHand = document.getElementById("minute-hand");
+  const secondHand = document.getElementById("second-hand");
 
   //获取时钟指针角度
-  const hourDeg = (360 / 12) * (hours + minutes / 60 + seconds / 3600 + milliseconds / 3600000);
-  const minuteDeg = (360 / 60) * (minutes + seconds / 60 + milliseconds / 60000);
+  const hourDeg =
+    (360 / 12) *
+    (hours + minutes / 60 + seconds / 3600 + milliseconds / 3600000);
+  const minuteDeg =
+    (360 / 60) * (minutes + seconds / 60 + milliseconds / 60000);
   const secondDeg = (360 / 60) * (seconds + milliseconds / 1000);
 
   //更新时针指针角度
-  hourHand.setAttribute('transform', `rotate(${hourDeg}, 250, 250)`);
-  minuteHand.setAttribute('transform', `rotate(${minuteDeg}, 250, 250)`);
-  secondHand.setAttribute('transform', `rotate(${secondDeg}, 250, 250)`);
+  hourHand.setAttribute("transform", `rotate(${hourDeg}, 250, 250)`);
+  minuteHand.setAttribute("transform", `rotate(${minuteDeg}, 250, 250)`);
+  secondHand.setAttribute("transform", `rotate(${secondDeg}, 250, 250)`);
+
+  //检查闹钟是否响起
+  alarms = JSON.parse(localStorage.getItem("alarmClocks"));
+  if (alarms) {
+    alarms.forEach(function (alarm) {
+      if (alarm.hour == hours && alarm.minute == minutes && seconds < 0.1) {
+        alert("闹钟响了:" + alarm.name);
+      }
+    });
+  }
 }
 
 setInterval(updateClock, 10); // 每10毫秒更新一次时钟，因此时钟显示为100帧
