ef477d95e4e196f71e2009f94d40c8b384af7db3
liuhaixu21 <liuhaixu21@mails.tsinghua.edu.cn>
2024-07-19 13:17:36 +0800

    增加了鼠标拖动时钟指针功能，以及一些公共接口

--- main.js
+++ main.js
@@ -1,26 +1,196 @@
-function updateClock() {
-  // 获取当前时间
-  const now = new Date();
-  const hours = now.getHours() % 12;
-  const minutes = now.getMinutes();
-  const seconds = parseFloat(now.getSeconds());
-  const milliseconds = parseFloat(now.getMilliseconds());
+// 表示是否是现实时间
+let realTime = true;
 
-  // 获取dom树节点
-  const hourHand = document.getElementById('hour-hand');
-  const minuteHand = document.getElementById('minute-hand');
-  const secondHand = document.getElementById('second-hand');
+// 表示是否拖动
+let dragHand = 0;
 
-  //获取时钟指针角度
-  const hourDeg = (360 / 12) * (hours + minutes / 60 + seconds/3600 + milliseconds / 3600000);
-  const minuteDeg = (360 / 60) * (minutes + seconds/60 + milliseconds/60000);
-  const secondDeg = (360 / 60) * (seconds + milliseconds/1000);
+// 表示三个指针的角度
+let hourDeg = 0;
+let minuteDeg = 0;
+let secondDeg = 0;
 
-  //更新时针指针角度
-  hourHand.setAttribute('transform', `rotate(${hourDeg}, 250, 250)`);
-  minuteHand.setAttribute('transform', `rotate(${minuteDeg}, 250, 250)`);
-  secondHand.setAttribute('transform', `rotate(${secondDeg}, 250, 250)`);
+// 定义虚拟时间
+// 与Date()具有一些相同的接口
+class vTime {
+	constructor(hours, minutes, seconds, milliseconds) {
+		this.hours = hours;
+		this.minutes = minutes;
+		this.seconds = seconds;
+		this.milliseconds = milliseconds;
+	}
+
+	addMilliseconds(dms) {
+		this.milliseconds += dms;
+		if (this.milliseconds >= 1000) {
+			this.seconds++;
+			this.milliseconds -= 1000;
+		}
+		if (this.seconds >= 60) {
+			this.minutes++;
+			this.seconds -= 60;
+		}
+		if (this.minutes >= 60) {
+			this.hours++;
+			this.minutes -= 60;
+		}
+		if (this.hours >= 24) {
+			this.hours -= 24;
+		}
+	}
+
+	getHours() {
+		return this.hours;
+	}
+
+	getMinutes() {
+		return this.minutes;
+	}
+
+	getSeconds() {
+		return this.seconds;
+	}
+
+	getMilliseconds() {
+		return this.milliseconds;
+	}
+
+	copyFrom(time) {
+		this.hours = time.getHours();
+		this.minutes = time.getMinutes();
+		this.seconds = time.getSeconds();
+		this.milliseconds = time.getMilliseconds();
+	}
 }
 
-setInterval(updateClock, 10); // 每10毫秒更新一次时钟，因此时钟显示为100帧
-updateClock(); // 初始加载时钟
+// 定义时钟刷新的频率
+const interval = 10;
+
+var vtime = new vTime(0, 0, 0, 0);
+
+function updateClock() {
+	// 当没有拖动时执行
+	if (!dragHand) {
+		// 更新虚拟时间
+		if (realTime) {
+			vtime.copyFrom(new Date());
+		} else {
+			vtime.addMilliseconds(interval);
+		}
+		// 获取当前时间
+		const now = realTime ? new Date() : vtime; // TODO
+		const hours = now.getHours() % 12;
+		const minutes = now.getMinutes();
+		const seconds = parseFloat(now.getSeconds());
+		const milliseconds = parseFloat(now.getMilliseconds());
+
+		// 获取dom树节点
+		const hourHand = document.getElementById('hour-hand');
+		const minuteHand = document.getElementById('minute-hand');
+		const secondHand = document.getElementById('second-hand');
+
+		// 获取时钟指针角度
+		hourDeg = (360 / 12) * (hours + minutes / 60 + seconds / 3600 + milliseconds / 3600000);
+		minuteDeg = (360 / 60) * (minutes + seconds / 60 + milliseconds / 60000);
+		secondDeg = (360 / 60) * (seconds + milliseconds / 1000);
+
+		//更新时针指针角度
+		hourHand.setAttribute('transform', `rotate(${hourDeg}, 250, 250)`);
+		minuteHand.setAttribute('transform', `rotate(${minuteDeg}, 250, 250)`);
+		secondHand.setAttribute('transform', `rotate(${secondDeg}, 250, 250)`);
+	}
+}
+
+// 增加鼠标拖动功能
+const handContainer = document.getElementById('clock_hand');
+const clock = document.getElementById('clock');
+
+handContainer.addEventListener('mousedown', (event) => {
+	const hand = event.target;
+	console.log('down');
+
+	switch (hand.id) {
+		case 'hour-hand-drag':
+			dragHand = 1;
+			break;
+		case 'minute-hand-drag':
+			dragHand = 2;
+			break;
+		case 'second-hand-drag':
+			dragHand = 3;
+			break;
+		default:
+			dragHand = 0;
+			break;
+	}
+});
+
+function slopeToDeg(dx, dy) {
+	let slope = Math.atan(dy / dx);
+	let deg = (slope * 180) / Math.PI + 90;
+	return dx < 0 ? deg + 180 : deg;
+}
+
+clock.addEventListener('mousemove', (event) => {
+	if (dragHand) {
+		const hand = event.target;
+
+		console.log('move');
+
+		// 判断是否点击到了旋炳
+		if (
+			(dragHand == 1 && hand.id != 'hour-hand-drag') ||
+			(dragHand == 2 && hand.id != 'minute-hand-drag') ||
+			(dragHand == 3 && hand.id != 'second-hand-drag')
+		) {
+			return;
+		}
+
+		let dx = event.clientX - (clock.getBoundingClientRect().left + 250);
+		let dy = event.clientY - (clock.getBoundingClientRect().top + 250);
+		let deg = slopeToDeg(dx, dy);
+		console.log(dx, dy, deg);
+
+		// 改变当前角度
+		switch (dragHand) {
+			case 1:
+				hourDeg = deg;
+				break;
+			case 2:
+				minuteDeg = deg;
+				break;
+			case 3:
+				secondDeg = deg;
+				break;
+			default:
+				break;
+		}
+
+		hand.parentElement.setAttribute('transform', `rotate(${deg}, 250, 250)`);
+		updateVtime();
+		console.log(vtime.getSeconds());
+	}
+});
+
+handContainer.addEventListener('mouseup', () => {
+	console.log('up');
+	if (dragHand) {
+		dragHand = 0;
+		realTime = false;
+
+		// 更新虚拟时间
+		updateVtime();
+	}
+});
+
+// 根据钟表指针更新虚拟时间
+function updateVtime() {
+	let hours = hourDeg / 30 - minuteDeg / 360;
+	let minute = minuteDeg / 6 - secondDeg / 360;
+	let second = Math.floor(secondDeg / 6);
+	let milliseconds = (secondDeg / 6 - second) * 1000;
+
+	vtime = new vTime(hours, minute, second, milliseconds);
+}
+
+setInterval(updateClock, interval); // 每10毫秒更新一次时钟，因此时钟显示为100帧
+updateClock(); // 初始加载时钟 ？这一行貌似可以删去？
