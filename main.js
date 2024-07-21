622853fc6ab59f5acb2a4a3033b129769af78cc3
Ming <ssz21@mails.tsinghua.edu.cn>
2024-07-21 11:33:20 +0800

    改名

--- main.js
+++ main.js
@@ -95,9 +95,10 @@ function updateClock() {
 
 		//更新数字时间显示
 		// const hoursNum=hours.
-		const timeString=`${String(hours.toFixed(0)).padStart(2,'0')} : ${String(minutes.toFixed(0)).padStart(2,'0')} : ${String(seconds.toFixed(0)).padStart(2,'0')}`;
-		document.getElementById('time-display').textContent=timeString;
-
+		const timeString = `${String(hours.toFixed(0)).padStart(2, '0')} : ${String(minutes.toFixed(0)).padStart(2, '0')} : ${String(
+			seconds.toFixed(0)
+		).padStart(2, '0')}`;
+		document.getElementById('time-display').textContent = timeString;
 
 		// 获取dom树节点
 		const hourHand = document.getElementById('hour-hand');
@@ -223,47 +224,48 @@ function updateVtime() {
 }
 
 //根据选择启用不同功能
-window.onload=function(){
-	var clockFunction = document.getElementById('clock-functions'); 
-	if(clockFunction){
-		clockFunction.addEventListener('change', (event) => { 
-			const selectedFunction = event.target.value;  
-			const timeInputHour = document.getElementById('time-input-hour');  
-			const timeInputMinute = document.getElementById('time-input-minute');  
-			const timeInputSecond = document.getElementById('time-input-second');  
-			const timeInputButton = document.getElementById('time-input-button');  
-	
-			if (selectedFunction === 'setTime') {  
-				timeInputHour.disabled = false; 
-				timeInputMinute.disabled = false;  
-				timeInputSecond.disabled = false;  
-				timeInputHour.style.display = 'block'; 
-				timeInputMinute.style.display = 'block'; 
-				timeInputSecond.style.display = 'block'; 
-				timeInputButton.style.display = 'block'; 
-			} else {  
-				timeInputHour.disabled = true; 
-				timeInputMinute.disabled = true;  
-				timeInputSecond.disabled = true; 
-				timeInputHour.style.display = 'none'; 
-				timeInputMinute.style.display = 'none'; 
-				timeInputSecond.style.display = 'none'; 
-				timeInputButton.style.display = 'none'; 
-			} 
-		})
+window.onload = function () {
+	var clockFunction = document.getElementById('clock-functions');
+	if (clockFunction) {
+		clockFunction.addEventListener('change', (event) => {
+			const selectedFunction = event.target.value;
+			const timeInputHour = document.getElementById('time-input-hour');
+			const timeInputMinute = document.getElementById('time-input-minute');
+			const timeInputSecond = document.getElementById('time-input-second');
+			const timeInputButton = document.getElementById('time-input-button');
+
+			if (selectedFunction === 'setTime') {
+				timeInputHour.disabled = false;
+				timeInputMinute.disabled = false;
+				timeInputSecond.disabled = false;
+				timeInputHour.style.display = 'block';
+				timeInputMinute.style.display = 'block';
+				timeInputSecond.style.display = 'block';
+				timeInputButton.style.display = 'block';
+			} else if (selectedFunction === 'alarm') {
+				location.href = 'alarm.html';
+			} else if (selectedFunction === 'stopwatch') {
+				location.href = 'stopwatch.html';
+			} else {
+				timeInputHour.disabled = true;
+				timeInputMinute.disabled = true;
+				timeInputSecond.disabled = true;
+				timeInputHour.style.display = 'none';
+				timeInputMinute.style.display = 'none';
+				timeInputSecond.style.display = 'none';
+				timeInputButton.style.display = 'none';
+			}
+		});
 	}
 
 	//通过时间输入框实现时分秒设置
-	function setTimeFromInput() {  
-		const hours = parseInt(document.getElementById('time-input-hour').value, 10); 
-		const minutes = parseInt(document.getElementById('time-input-minute').value, 10);  
-		const seconds = parseInt(document.getElementById('time-input-second').value, 10);  
-	
-		realTime=false;
+	function setTimeFromInput() {
+		const hours = parseInt(document.getElementById('time-input-hour').value, 10);
+		const minutes = parseInt(document.getElementById('time-input-minute').value, 10);
+		const seconds = parseInt(document.getElementById('time-input-second').value, 10);
+
+		realTime = false;
 		vtime = new vTime(hours, minutes, seconds, 0);
-	}  
-	document.getElementById('time-input-button').addEventListener('click', setTimeFromInput);  
-}
-
-
-
+	}
+	document.getElementById('time-input-button').addEventListener('click', setTimeFromInput);
+};
