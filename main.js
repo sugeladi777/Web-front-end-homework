c71c11dceddc8d03f4ab0f623c5957f7e2a2f45c
Ming <ssz21@mails.tsinghua.edu.cn>
2024-07-21 15:41:59 +0800

    基本完成

--- main.js
+++ main.js
@@ -92,13 +92,15 @@ function updateClock() {
 		const minutes = now.getMinutes();
 		const seconds = parseFloat(now.getSeconds());
 		const milliseconds = parseFloat(now.getMilliseconds());
+		const hoursDisplay = now.getHours()% 24;
+		if(hoursDisplay<0){
+			hoursDisplay=0;
+		}
 
 		//更新数字时间显示
-		// const hoursNum=hours.
-		const timeString = `${String(hours.toFixed(0)).padStart(2, '0')} : ${String(minutes.toFixed(0)).padStart(2, '0')} : ${String(
-			seconds.toFixed(0)
-		).padStart(2, '0')}`;
-		document.getElementById('time-display').textContent = timeString;
+		const timeString=`${String(hoursDisplay.toFixed(0)).padStart(2,'0')} : ${String(minutes.toFixed(0)).padStart(2,'0')} : ${String(seconds.toFixed(0)).padStart(2,'0')}`;
+		document.getElementById('time-display').textContent=timeString;
+
 
 		// 获取dom树节点
 		const hourHand = document.getElementById('hour-hand');
@@ -159,42 +161,48 @@ function setDrag() {
 
 			console.log('move');
 
-			// 判断是否点击到了旋炳
-			if (
-				(dragHand == 1 && hand.id != 'hour-hand-drag') ||
-				(dragHand == 2 && hand.id != 'minute-hand-drag') ||
-				(dragHand == 3 && hand.id != 'second-hand-drag')
-			) {
-				return;
-			}
-
 			let dx = event.clientX - (clock.getBoundingClientRect().left + 250);
 			let dy = event.clientY - (clock.getBoundingClientRect().top + 250);
 			let deg = slopeToDeg(dx, dy);
-			console.log(dx, dy, deg);
+			let ddeg = 0;
+			// console.log(dx, dy, deg);
 
-			// 改变当前角度
-			switch (dragHand) {
-				case 1:
-					hourDeg = deg;
-					break;
-				case 2:
-					minuteDeg = deg;
-					break;
-				case 3:
-					secondDeg = deg;
-					break;
-				default:
-					break;
+			let degs = [hourDeg, minuteDeg, secondDeg];
+
+			ddeg = deg - degs[dragHand - 1];
+
+			// 特殊情况处理，度过0
+			let clockwise = 0;
+			if (0 < deg && deg < 30 && 330 < degs[dragHand - 1] && degs[dragHand - 1] < 360) {
+				console.log('zheng');
+				clockwise = 1;
+			} else if (330 < deg && deg < 360 && 0 < degs[dragHand - 1] && degs[dragHand - 1] < 30) {
+				console.log('ni');
+				clockwise = -1;
 			}
+			console.log(deg, degs[dragHand - 1], ddeg);
 
-			hand.parentElement.setAttribute('transform', `rotate(${deg}, 250, 250)`);
+			let linkage = handLinkage(ddeg, dragHand, clockwise);
+			hourDeg += linkage[0];
+			minuteDeg += linkage[1];
+			secondDeg += linkage[2];
+
+			// 获取dom树节点
+			const hourHand = document.getElementById('hour-hand');
+			const minuteHand = document.getElementById('minute-hand');
+			const secondHand = document.getElementById('second-hand');
+
+			//更新时针指针角度
+			hourHand.setAttribute('transform', `rotate(${hourDeg}, 250, 250)`);
+			minuteHand.setAttribute('transform', `rotate(${minuteDeg}, 250, 250)`);
+			secondHand.setAttribute('transform', `rotate(${secondDeg}, 250, 250)`);
+
 			updateVtime();
 			console.log(vtime.getSeconds());
 		}
 	});
 
-	handContainer.addEventListener('mouseup', () => {
+	clock.addEventListener('mouseup', () => {
 		console.log('up');
 		if (dragHand) {
 			dragHand = 0;
@@ -213,6 +221,30 @@ function slopeToDeg(dx, dy) {
 	return dx < 0 ? deg + 180 : deg;
 }
 
+// 指针联动
+function handLinkage(ddeg, dragHand, clockwise) {
+	if (!dragHand) {
+		return;
+	}
+	res = [0, 0, 0];
+
+	ddeg += 360 * clockwise;
+	switch (dragHand) {
+		case 1:
+			res = [ddeg - 360 * clockwise, ddeg * 60, ddeg * 60 * 60];
+			break;
+		case 2:
+			res = [ddeg / 60, ddeg - 360 * clockwise, ddeg * 60];
+			break;
+		case 3:
+			res = [ddeg / 60 / 60, ddeg / 60, ddeg - 360 * clockwise];
+			break;
+		default:
+			break;
+	}
+	return res;
+}
+
 // 根据钟表指针更新虚拟时间
 function updateVtime() {
 	let hours = hourDeg / 30 - minuteDeg / 360;
@@ -224,41 +256,67 @@ function updateVtime() {
 }
 
 //根据选择启用不同功能
-window.onload = function () {
-	var clockFunction = document.getElementById('clock-functions');
-	if (clockFunction) {
-		clockFunction.addEventListener('change', (event) => {
-			const selectedFunction = event.target.value;
-			const timeInputHour = document.getElementById('time-input-hour');
-			const timeInputMinute = document.getElementById('time-input-minute');
-			const timeInputSecond = document.getElementById('time-input-second');
-			const timeInputButton = document.getElementById('time-input-button');
+window.onload=function(){
+	var clockFunction = document.getElementById('clock-functions'); 
+	if(clockFunction){
+		clockFunction.addEventListener('change', (event) => { 
+			const selectedFunction = event.target.value;  
+			const timeInputHour = document.getElementById('time-input-hour');  
+			const timeInputMinute = document.getElementById('time-input-minute');  
+			const timeInputSecond = document.getElementById('time-input-second');  
+			const timeInputButton = document.getElementById('time-input-button');  
+	
+			if (selectedFunction === 'setTime') {  
+				timeInputHour.disabled = false; 
+				timeInputMinute.disabled = false;  
+				timeInputSecond.disabled = false;  
+				timeInputHour.style.display = 'block'; 
+				timeInputMinute.style.display = 'block'; 
+				timeInputSecond.style.display = 'block'; 
+				timeInputButton.style.display = 'block'; 
+			} else {  
+				timeInputHour.disabled = true; 
+				timeInputMinute.disabled = true;  
+				timeInputSecond.disabled = true; 
+				timeInputHour.style.display = 'none'; 
+				timeInputMinute.style.display = 'none'; 
+				timeInputSecond.style.display = 'none'; 
+				timeInputButton.style.display = 'none'; 
+			} 
+		})
+			// const selectedFunction = event.target.value;  
+			const timeInputHour = document.getElementById('time-input-hour');  
+			const timeInputMinute = document.getElementById('time-input-minute');  
+			const timeInputSecond = document.getElementById('time-input-second');  
+			// const timeInputButton = document.getElementById('time-input-button');  
 
-			if (selectedFunction === 'setTime') {
-				timeInputHour.disabled = false;
-				timeInputMinute.disabled = false;
-				timeInputSecond.disabled = false;
-				timeInputHour.style.display = 'block';
-				timeInputMinute.style.display = 'block';
-				timeInputSecond.style.display = 'block';
-				timeInputButton.style.display = 'block';
-			} else if (selectedFunction === 'alarm') {
-				location.href = 'alarm.html';
-			} else if (selectedFunction === 'stopwatch') {
-				location.href = 'stopwatch.html';
-			} else if (selectedFunction === 'timer') {
-				location.href = 'timer.html';
-			} else {
-				timeInputHour.disabled = true;
-				timeInputMinute.disabled = true;
-				timeInputSecond.disabled = true;
-				timeInputHour.style.display = 'none';
-				timeInputMinute.style.display = 'none';
-				timeInputSecond.style.display = 'none';
-				timeInputButton.style.display = 'none';
+		timeInputHour.addEventListener("input", function (e) {
+			console.log("inputhour");
+			if (e.target.value < 0) {
+			  e.target.value = 0;
+			} else if (e.target.value > 23) {
+			  e.target.value = 23;
 			}
-		});
+		  });
+	
+		  timeInputMinute.addEventListener("input", function (e) {
+			if (e.target.value < 0) {
+			  e.target.value = 0;
+			} else if (e.target.value > 23) {
+			  e.target.value = 23;
+			}
+		  });
+	
+		  timeInputSecond.addEventListener("input", function (e) {
+			if (e.target.value < 0) {
+			  e.target.value = 0;
+			} else if (e.target.value > 23) {
+			  e.target.value = 23;
+			}
+		  });
 	}
+
+	
 
 	//通过时间输入框实现时分秒设置
 	function setTimeFromInput() {
@@ -271,3 +329,23 @@ window.onload = function () {
 	}
 	document.getElementById('time-input-button').addEventListener('click', setTimeFromInput);
 };
+	function setTimeFromInput() {  
+		const hours = parseInt(document.getElementById('time-input-hour').value, 10); 
+		const minutes = parseInt(document.getElementById('time-input-minute').value, 10);  
+		const seconds = parseInt(document.getElementById('time-input-second').value, 10);  
+		if (!isNaN(hours) && hours >= 0 && hours <= 23 &&  
+        !isNaN(minutes) && minutes >= 0 && minutes <= 59 &&  
+        !isNaN(seconds) && seconds >= 0 && seconds <= 59){
+			realTime=false;
+			vtime = new vTime(hours, minutes, seconds, 0);
+		}else{
+			alert("时间格式有误");
+		}
+
+		
+	}  
+	document.getElementById('time-input-button').addEventListener('click', setTimeFromInput);  
+}
+
+
+
